5ce03b38421e6900d4deb99fe2351cc3c0611f70
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-01 05:11:29 +0900

    Feat: Set up routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Detail from './pages/Detail';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Header from './components/Header';
 import { useEffect } from 'react';
 import { fetchPokemonDataById } from './RTK/thunk';
+import { useDispatch } from 'react-redux';
 
 const MAX_ID = 151;
 
 function App() {
   const dispatch = useDispatch();
-  const pokemonData = useSelector((state) => state.pokemon.data);
   useEffect(() => {
     dispatch(fetchPokemonDataById(MAX_ID));
   }, [dispatch]);
-  console.log(pokemonData);
-  return <></>;
+  return (
+    <>
+      <Header />
+      <section className='mt-36 p-5 flex justify-center'>
+        <Routes>
+          <Route path={'/'} element={<Main />} />
+          <Route path={'/detail/:id'} element={<Detail />} />
+          <Route path={'/search'} element={<Search />} />
+          <Route path={'/favorite'} element={<Favorites />} />
+        </Routes>
+      </section>
+    </>
+  );
 }
 
 export default App;
