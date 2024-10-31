0939ceabd73b741445bdb0202268b457e9234420
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-01 05:16:30 +0900

    Design: Set up global style and add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      galmuri7: 'Galmuri7',
+      galmuri9: 'Galmuri9',
+      galmuri11: 'Galmuri11',
+      galmuri14: 'Galmuri14',
+      dungGeunMo: 'DungGeunMo',
+    },
   },
   plugins: [],
 };
