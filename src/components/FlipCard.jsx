f4414310bbd121e3b1c933e56d8f2ee67d60e975
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-01 20:50:16 +0900

    Implement detail page

---
+++ src/components/FlipCard.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const FlipImageContainer = styled.div`
+  transform-style: preserve-3d;
+  width: 300px;
+  height: 300px;
+  position: relative;
+  transition: 0.5s;
+  transform: ${({ flipped }) =>
+    flipped === 'flipped' ? 'rotateY(180deg)' : 'rotateY(0deg)'};
+`;
+
+const FrontImage = styled.img`
+  backface-visibility: hidden;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const BackImage = styled.img`
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+export default function FlipCard({ frontImage, backImage }) {
+  const [flipped, setFlipped] = useState(false);
+  return (
+    <div className='relative'>
+      <FlipImageContainer flipped={flipped ? 'flipped' : 'notFlipped'}>
+        <FrontImage src={frontImage} />
+        <BackImage src={backImage} />
+      </FlipImageContainer>
+      <button
+        className='font-galmuri7 absolute top-7 right-10 bg-zinc-900 text-zinc-200 px-1'
+        onClick={() => setFlipped(!flipped)}
+      >
+        flip!
+      </button>
+    </div>
+  );
+}
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,18 +1,20 @@
 import { useSelector } from 'react-redux';
 import { selectPokemonById } from '../RTK/selector';
 import { useParams } from 'react-router-dom';
+import FavoriteButton from '../components/FavoriteButton';
+import FlipCard from '../components/FlipCard';
 
 export default function Detail() {
   const { id } = useParams();
   const pokemon = useSelector(selectPokemonById(Number(id)));
-
   return (
-    <section className='flex flex-col items-center my-10 border border-zinc-200 rounded-md w-96 h-[500px] p-3'>
+    <section className='flex flex-col bg-zinc-200 text-zinc-900 items-center my-10 border border-zinc-200 rounded-md w-96 h-[500px] p-3 relative'>
       <h1 className='text-3xl font-dungGeunMo'>{pokemon.name}</h1>
       <pre className='font-sans text-center my-5 font-galmuri14'>
         {pokemon.description}
       </pre>
-      <img className='flex-grow' src={pokemon.frontImage} alt={pokemon.name} />
+      <FlipCard frontImage={pokemon.frontImage} backImage={pokemon.backImage} />
+      <FavoriteButton id={id} />
     </section>
   );
 }
